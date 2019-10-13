ab699416b4ea85c31e00b62c8407053af51e188a
Karol Andracki <andracki@gmail.com>
2019-10-13 11:20:55 +0200

    js- on click-infobox- scratch

---
+++ photolinker.js
@@ -0,0 +1,13 @@
+function chat() {
+    var plik = '<img src="bckgrnd1.jpeg" alt="">';
+    document.getElementById("infobox").innerHTML = plik;
+
+
+
+}
+
+function wishlist() {
+    var plik = '<img src="2.png" alt="">'
+    document.getElementById("infobox").innerHTML = plik;
+}
+
