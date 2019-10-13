4495e68d7fdde5a2694225f77a22fb8fc3ceeadb
Karol Andracki <andracki@gmail.com>
2019-10-13 11:48:06 +0200

    onload function, onclick - img changed for <p>

--- photolinker.js
+++ photolinker.js
@@ -1,5 +1,14 @@
+
+function general() {
+    var plik = '<p> To know more about our aplication, click on the link for more information </p>';
+    document.getElementById("infobox").innerHTML = plik;
+
+}
+
+
+
 function chat() {
-    var plik = '<img src="bckgrnd1.jpeg" alt="">';
+    var plik = '<p> lorem ipsum dalsze info </p>';
     document.getElementById("infobox").innerHTML = plik;
 
 
@@ -7,7 +16,7 @@ function chat() {
 }
 
 function wishlist() {
-    var plik = '<img src="2.png" alt="">'
+    var plik = '<p> informacje <br> wiecej informacji <br> kolejny tekst</p>'
     document.getElementById("infobox").innerHTML = plik;
 }
 
