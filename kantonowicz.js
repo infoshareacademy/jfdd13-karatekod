0bfdeeb9acb6f8e6e16739456dde0f5aa455b7d5
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-10-24 15:51:49 +0200

    added linkedin logo

--- kantonowicz.js
+++ kantonowicz.js
@@ -9,6 +9,7 @@ let windowPositionTop = window.pageYOffset */
 
 // GRAB THE IMAGES 
 
+let antek = document.querySelectorAll('.emoji1')[0]
 
 
 let michalako = document.querySelectorAll('.emoji1')[1].offsetTop
@@ -16,14 +17,30 @@ let juhanilao = document.querySelectorAll('.emoji1')[2].offsetTop
 let andracki = document.querySelectorAll('.emoji1')[3].offsetTop
 
 function scrollDown1() {
+    
     let antonowicz = document.querySelectorAll('.emoji1')[0].offsetTop
     if (Math.round(antonowicz/100)*100-100 === Math.round(document.documentElement.scrollTop/100)*100){
+     
     console.log('ok')
-    
+
+
+ 
 }
 }
 
+
+let karolek = document.querySelectorAll('.team-info-photos figure')[0]
+karolek.style.backgroundColor = 'blue'
+antek.style.display = 'initial'
 
 
 
 document.addEventListener('scroll', scrollDown1)
+
+
+
+
+
+
+
+
